593d4017a56568cc030797badef95c54b8edb03b
Fatih <fatihtekce1632@gmail.com>
2025-01-08 03:44:22 +0300

    database added

--- src/Movie.java
+++ src/Movie.java
@@ -1,18 +1,27 @@
 public class Movie {
     private int id;
     private String title;
+    private int year;
     private String genre;
     private String summary;
     private byte[] posterImage;
-    private double price;
 
-    public Movie(int id, String title, String genre, String summary, byte[] posterImage, double price) {
+    public Movie(){
+        this.id = 0;
+        this.title = null;
+        this.year = 0;
+        this.genre = null;
+        this.summary = null;
+        this.posterImage = null;
+    }
+
+    public Movie(int id, String title, int year, String genre,  String summary, byte[] posterImage) {
         this.id = id;
         this.title = title;
+        this.year = year;
         this.genre = genre;
         this.summary = summary;
         this.posterImage = posterImage;
-        this.price = price;
     }
 
     // Getter ve Setter metodları
@@ -21,6 +30,9 @@ public class Movie {
 
     public String getTitle() { return title; }
     public void setTitle(String title) { this.title = title; }
+
+    public int getYear() { return year; }
+    public void setYear(int year) { this.year = year; }
 
     public String getGenre() { return genre; }
     public void setGenre(String genre) { this.genre = genre; }
@@ -31,6 +43,5 @@ public class Movie {
     public byte[] getPosterImage() { return posterImage; }
     public void setPosterImage(byte[] posterImage) { this.posterImage = posterImage; }
 
-    public double getPrice() { return price; }
-    public void setPrice(double price) { this.price = price; }
+
 }
